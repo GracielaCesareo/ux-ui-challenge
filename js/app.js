ed6a0afbbe10f9c188ec9c9b8df45151bc86b890
GCC <bbvagc@GCCs-MacBook-Pro.local>
2020-11-03 01:05:16 -0600

    Fixes & features App

--- js/app.js
+++ js/app.js
@@ -1,9 +1,32 @@
+/* Dynamic Projects Load*/ 
+
+import projectsData from './data.js';
+
+const cardTemplate = (card) => {
+
+    return `
+            <div class="card-project">
+                <span class="mark-project"><i class="fas fa-star"></i></span>
+                <p class="name-project">${card.title}</p>
+                <p class="date-project">${card.date}</p>
+                <p class="description-project">${card.description}</p>
+                <p class="estimation-project">
+                Total Estimates: <span>${card.estimation}</span>  
+                </p>
+            </div>
+    `
+
+}
+
+document.getElementById('contentProjects').innerHTML = `${projectsData.map(cardTemplate).join('')}`
+
+/* Search interaction for desktop */ 
 const searchButton = document.getElementById('searchButton') 
 
-const toogleClassButton = () => {
+const toggleClassButtonSearch = () => {
     const element    = document.getElementById('inputSearch');
     const searchIcon = document.getElementsByClassName('fa-search')[0];
-    const closeIcon  = document.getElementsByClassName('fa-times')[0];
+    const closeIcon  = document.getElementsByClassName('fa-times')[1];
 
     element.classList.toggle("active");
 
@@ -14,4 +37,33 @@ const toogleClassButton = () => {
     (closeIcon.style.display = 'none', searchIcon.style.display = 'block', element.value = "" )
 }
 
-searchButton.addEventListener('click', toogleClassButton)
+searchButton.addEventListener('click', toggleClassButtonSearch)
+
+/* Sing up button container interaction */ 
+const chevronButton = document.getElementById('downArrow')
+
+const toggleClassButtonArrow = () =>{
+    const containerButton  = document.getElementsByClassName('cta-sing-out')[0]
+
+    chevronButton.classList.toggle("open")
+}
+
+chevronButton.addEventListener('click', toggleClassButtonArrow)
+
+/* Mobile menu interaction */ 
+const mobileMenuButton = document.getElementById('mobileMenu')
+
+const toggleClassMobileButton = () => {
+    const navigationContainer = document.getElementsByClassName('navigation-container')[0]
+    const closeIconButton     = document.getElementsByClassName('close')[0]
+    const menuIcon            = document.getElementsByClassName('menu')[0]
+
+    mobileMenuButton.classList.toggle("open-menu")
+    mobileMenuButton.classList.contains('open-menu')
+    ?
+    (navigationContainer.style.opacity = 1, closeIconButton.style.display = 'block', menuIcon.style.display = 'none')
+    :
+    (navigationContainer.style.opacity = 0, closeIconButton.style.display = 'none', menuIcon.style.display = 'block')
+}
+
+mobileMenuButton.addEventListener('click', toggleClassMobileButton)
---
+++ js/data.js
@@ -0,0 +1,37 @@
+const projectsData = [
+    {
+        title: "Test Project",
+        date: "Nov 3 2020",
+        description: "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat ",
+        estimation: 8
+    },
+    {
+        title: "Client's Project With Stories, New Project Estimation new",
+        date: "Oct 19 2020",
+        description: "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat ",
+        estimation: 12
+    },
+    {
+        title: "Test Project With Estimates",
+        date: "Oct 12 2020",
+        description: "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat ",
+        estimation: 18
+    },
+    {
+        title: "E-commerce Project",
+        date: "Oct 05 2020",
+        description: "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat ",
+        estimation: 6
+    },
+    {
+        title: "Another Client's Project",
+        date: "Sep 6 2020",
+        description: "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat ",
+        estimation: 9
+    }
+      
+    
+  
+  ];
+  
+  export default projectsData;
